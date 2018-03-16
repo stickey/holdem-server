445a1ae8204c0756f12ce23a85a68c495f715439
Matt Stocke <mattstocke@Matts-MacBook-Air.local>
2018-03-15 22:19:20 -0500

    Added pot size property/column

--- grails-app/domain/holdem/server/Game.groovy
+++ grails-app/domain/holdem/server/Game.groovy
@@ -2,11 +2,14 @@ package holdem.server
 
 import grails.rest.Resource
 
-@Resource(uri="/game")
+@Resource(uri = "/game")
 class Game {
+
+    Float pot = 0.00F
 
     Date dateCreated = new Date()
 
     static constraints = {
+        pot nullable: false
     }
 }
