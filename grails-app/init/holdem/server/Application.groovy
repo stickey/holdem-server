0cffe82847e1dcc2c95a9c5c71342b72f76909d8
Matt Stocke <mattstocke@Matts-MacBook-Air.local>
2018-03-15 21:13:04 -0500

    Initial commit

---
+++ grails-app/init/holdem/server/Application.groovy
@@ -0,0 +1,10 @@
+package holdem.server
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
---
+++ grails-app/init/holdem/server/BootStrap.groovy
@@ -0,0 +1,9 @@
+package holdem.server
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
